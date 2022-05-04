d760315783946eb7e7a869f52e75397115022920
Tanya <tanaybrown@gmail.com>
2022-05-03 17:58:39 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+const para = document.querySelector('#append')
+function submitData(userName, userEmail) {
+    return fetch('http://localhost:3000/users',{
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail,
+        })
+
+     })
+     .then(response => response.json())
+     .then(users=>{ const newId = document.createTextNode(users.id)
+         return para.appendChild(newId)}
+         )
+      .catch(error=>para.textContent += error.message)
+    }
